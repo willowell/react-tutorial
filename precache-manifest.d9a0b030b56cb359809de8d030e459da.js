e205521a3da100a251f116c3766ab447f5789ed5
William Howell <wlm.howell@gmail.com>
2019-12-28 00:38:24 -0500

    Updates

---
+++ precache-manifest.d9a0b030b56cb359809de8d030e459da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00409785d4e0038a1d3902295ca6914f",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "938ecdb3f98caac99054",
+    "url": "/react-tutorial/static/css/main.08011531.chunk.css"
+  },
+  {
+    "revision": "3da17989d5c5da24ec10",
+    "url": "/react-tutorial/static/js/2.47b8276d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-tutorial/static/js/2.47b8276d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "938ecdb3f98caac99054",
+    "url": "/react-tutorial/static/js/main.423c2fea.chunk.js"
+  },
+  {
+    "revision": "8d76b84dd1ea32b60fde",
+    "url": "/react-tutorial/static/js/runtime-main.f01b29f8.js"
+  }
+]);
